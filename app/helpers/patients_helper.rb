9138443190dde56f1766492c93eefa80e3961ebe
mirandov <tremors224@gmail.com>
2017-12-15 23:06:31 +0300

    options_for_select

--- app/helpers/patients_helper.rb
+++ app/helpers/patients_helper.rb
@@ -1,2 +1,22 @@
 module PatientsHelper
+
+  def place_work_for_select
+    result = PlaceWork.all.map {|p| [p.job_name, p.id]}
+  end
+
+  def site_for_select
+    result = Site.all.map {|s| [s.site_name, s.id]}
+  end
+
+  def city_for_select
+    result = City.all.map {|c| [c.city_name, c.id]}
+  end
+
+  def street_for_select
+    result = Street.all.map {|s| [s.street_name, s.id]}
+  end
+
+  def house_for_select
+    result = House.all.map {|h| [h.house_number, h.id]}
+  end
 end
